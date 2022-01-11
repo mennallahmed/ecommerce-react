7680d97d42c9f052a009132edb593a6d9d921966
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-01-11 19:17:01 +0200

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import Annoucement from "../components/Annoucement"
+
+
+
+const Home = () => {
+  return(
+    <div>
+      <Annoucement/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
