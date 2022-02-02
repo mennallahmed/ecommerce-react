9c275d841309efbd79bb8c9bdacbf03155064129
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-02-02 02:06:22 +0200

    JWT implemetation

--- src/ECOMMERCEAPI/routes/auth.js
+++ src/ECOMMERCEAPI/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req,res)=>{
@@ -29,9 +30,16 @@ router.post("/login", async (req,res)=>{
     const pass = hashedPassword.toString(CryptoJS.enc.Utf8);
     // Check if the password is correct
     pass !== req.body.password && res.status(401).json("Wrong Credentials");
+
+    const accessToken = jwt.sign({
+      id: user._id, isAdmin: user.isAdmin,
+     }, 
+     process.env.JWT_KEY,
+     { expiresIn:"3d"}
+    )
     // Separate the password and store the rest of the user data
     const {password, ...others} = user._doc;
-    res.status(200).json(others);
+    res.status(200).json({...others, accessToken});
 
   }catch(error){
     res.status(500).json(error);
