461a063b9f776662bda01689e09616f437a22f57
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-02-03 01:35:46 +0200

    Add cart CRUD operoperations with JWT

--- src/ECOMMERCEAPI/index.js
+++ src/ECOMMERCEAPI/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+
 
 dotenv.config();
 
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+
 
 app.listen(process.env.PORT || 5000,()=>{
    console.log("Server is running!");
